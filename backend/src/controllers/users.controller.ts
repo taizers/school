482e3fe78b9a5abdb9df0fefad15f7e749d81f66
taizers <sasha.sachok@gmail.com>
2022-11-05 22:23:17 +0300

    backend: add routes for update user and profile

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -3,13 +3,14 @@ import {
   findPaginatedUsers,
   findUser,
   findUsers,
+  getUser,
   updateUser,
 } from '../services/db/users.services';
 import { createUser } from '../services/db/auth.services';
 import { customResponse } from '../helpers/responce';
 import logger from '../helpers/logger';
 import { Op } from 'sequelize';
-import { UnProcessableEntityError } from '../helpers/error';
+import { BadCredentialsError, DontHaveAccessError, UnProcessableEntityError } from '../helpers/error';
 import {
   SearchMembersRequest,
   createUserRequest,
@@ -17,6 +18,7 @@ import {
 } from '../types/requests/users.request.type';
 import { ParamsIdRequest } from '../types/requests/global.request.type';
 import uuid = require('uuid');
+import bcrypt from 'bcrypt';
 
 export const getUserAction = async (
   req: ParamsIdRequest,
@@ -111,6 +113,65 @@ export const uploadUserAvatarAction = async (
     return customResponse(res, 200, user);
   } catch (err) {
     logger.error('Upload User Avatar Action - Cannot get users', err);
+    next(err);
+  }
+};
+
+export const updateUserAction = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+
+  const { id, role: userRole } = req.user;
+  const { post, role, username, group_id } = req.body;
+
+  logger.info(
+    `Update User Action: { userId: ${id}, post: ${post}, role: ${role}, username: ${username}, group_id: ${group_id} } `
+  );
+
+  try {
+    if (userRole !== 'admin') {
+      throw new DontHaveAccessError();
+    }
+    const user = await updateUser(id, { post, role, username, group_id });
+
+    return customResponse(res, 200, user);
+  } catch (err) {
+    logger.error('update User Action - Cannot update user', err);
+    next(err);
+  }
+};
+
+export const updateUserProfileAction = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.user;
+  const { username, new_password, old_password } = req.body;
+
+  logger.info(
+    `Update User Action: { userId: ${id}, username: ${username} } `
+  );
+
+  try {
+    const oldUser = await getUser({id});
+
+    const isPasswordsEqual = await bcrypt.compare(old_password, oldUser.password);
+
+    if (!isPasswordsEqual) {
+      throw new BadCredentialsError('Bad password');
+    }
+
+    const encryptedNewPassword = await bcrypt.hash(new_password, 10);
+
+
+    const user = await updateUser(id, { username, password: encryptedNewPassword });
+
+    return customResponse(res, 200, user);
+  } catch (err) {
+    logger.error('update User Action - Cannot update user', err);
     next(err);
   }
 };
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import {
   getUserAction,
   createUserAction,
-  getUsersAction
+  getUsersAction,
+  updateUserAction,
+  updateUserProfileAction,
 } from '../controllers/users.controller';
 import { paramsIdValidation } from '../validations/global.validation';
 import { createUserValidation, getUsersValidation, updateProfileValidation, updateUserValidation } from '../validations/users.validation';
@@ -12,7 +14,7 @@ const router = express.Router();
 router.get('/:id', paramsIdValidation, getUserAction);
 router.post('/', createUserValidation, createUserAction);
 router.get('/', getUsersValidation, getUsersAction);
-// router.put('/profile/', updateProfileValidation, getUsersAction);
-// router.put('/', updateUserValidation, getUsersAction);
+router.put('/profile/', updateProfileValidation, updateUserProfileAction);
+router.put('/', updateUserValidation, updateUserAction);
 
 export default router;
